3a11520753ee29deb1890e6d8e38f9511e38b31d
byol-han <byolhandev@gmail.com>
2025-06-26 02:45:18 -0700

    feat: create about page

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from '@react-router/dev/routes';
 export default [
   index('common/pages/home-page.tsx'),
   route('/profile', 'features/users/pages/profile-page.tsx'),
+  route('/about', 'features/users/pages/about-page.tsx'),
 ] satisfies RouteConfig;
