8acc4f44c21064c6d99f59806647b2bb0c839f7b
byol-han <byolhandev@gmail.com>
2025-07-06 01:18:33 -0700

    add SNS in team member section

--- app/common/components/team-member.tsx
+++ app/common/components/team-member.tsx
@@ -1,3 +1,6 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { SiX } from 'react-icons/si';
+
 export default function TeamMember() {
   const memberData = [
     {
@@ -5,6 +8,9 @@ export default function TeamMember() {
       name: '김가람',
       image: '/images/profile_garam.jpeg',
       alt: 'profile_garam',
+      github: 'https://github.com/iizz',
+      linkedin: 'https://www.linkedin.com/in/garamkim-dev/',
+      email: 'mailto:garamkim.dev@gmail.com',
       description:
         "I'm a Full-stack Developer with 7 years of experience, with a strong focus on frontend development over the past 3 years using React.js, TypeScript, and Flutter. I specialize in turning complex ideas into intuitive, high-performing user experiences that drive real business outcomes. Recently, I've been diving into Three.js to explore the world of 3D graphics on the web, expanding my creative and technical capabilities in immersive user interface design.",
     },
@@ -13,14 +19,20 @@ export default function TeamMember() {
       name: '박소정',
       image: '/images/profile_sojeong.jpeg',
       alt: 'profile_sojeong',
+      github: 'https://github.com/sojeongsojeong-park',
+      linkedin: 'https://www.linkedin.com/in/sojeong-park-fedeveloper/',
+      email: 'mailto:psoj90972@gmail.com',
       description:
         '👩🏻‍💻𝙀𝙣𝙩𝙝𝙪𝙨𝙞𝙖𝙨𝙩𝙞𝙘 𝙎𝙤𝙛𝙩𝙬𝙖𝙧𝙚 𝙀𝙣𝙜𝙞𝙣𝙚𝙚𝙧 with over 2 years of working experience in the e-commerce industry. I focus on transforming web experiences into meaningful user journeys. Constantly exploring new ways to enhance user interaction and digital accessibility with modern technologies.',
     },
     {
       order: 1,
       name: '한별',
-      image: '/images/profile_byol.jpeg',
+      image: '/images/avatar_byol.jpeg',
       alt: 'profile_byol',
+      github: 'https://github.com/byol-han',
+      linkedin: 'https://www.linkedin.com/in/byol-han/',
+      email: 'mailto:byolhandev@gmail.com',
       description:
         'I’m a Frontend Developer with a strong foundation in HTML, CSS, JavaScript and modern frameworks like React and Vue.js. With hands-on experience in SASS, Tailwind CSS, Bootstrap, Material UI, and Quasar, I focus on building seamless, user-centric web applications that deliver both functionality and aesthetic appeal.',
     },
@@ -40,7 +52,7 @@ export default function TeamMember() {
       <div className='w-full h-auto pb-[100px]'></div>
       {memberData.map((item, key) => (
         <div
-          className='relative flex flex-col items-center mb-[50px] md:mb-[100px] px-4'
+          className='relative flex items-center justify-center mb-[50px] md:mb-[100px] px-4'
           key={key}
         >
           <div className='flex flex-col gap-5 md:flex-row md:gap-8 lg:w-5xl md:w-3xl'>
@@ -51,9 +63,38 @@ export default function TeamMember() {
             >
               <div className='flex flex-col max-w-lg gap-8 md:gap-[60px] m-auto px-2 md:px-0'>
                 <div className='flex flex-col gap-4 text-center md:text-left'>
-                  <h3 className='text-black text-2xl md:text-3xl'>
-                    {item.name}
-                  </h3>
+                  <div className='flex gap-4 items-center justify-center'>
+                    <h3 className='text-black text-2xl md:text-3xl'>
+                      {item.name}
+                    </h3>
+                    <div className='flex justify-center items-center gap-4'>
+                      <a
+                        href={item.github}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        aria-label='LinkedIn'
+                      >
+                        <FaGithub size={24} />
+                      </a>
+                      <a
+                        href={item.linkedin}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        aria-label='LinkedIn'
+                      >
+                        <FaLinkedin size={24} />
+                      </a>
+
+                      <a
+                        href={item.email}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                        aria-label='Email'
+                      >
+                        <FaEnvelope size={22} />
+                      </a>
+                    </div>
+                  </div>
                   <p className='text-black text-md'>{item.description}</p>
                 </div>
               </div>
